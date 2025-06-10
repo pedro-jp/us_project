7f42eb1ce363a007fa41203f4d8279e95ce4f909
João Pedro <joaopedroc035@gmail.com>
2025-06-10 11:33:18 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,150 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <main className='main'>
+        <section className='images-container'>
+          <div className='first-content'>
+            <div className='main-images'>
+              <div className='main-images-content'>
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>
+              </div>
+            </div>
+            <div className='vertical-carousel'>
+              <div className='vertical-carousel-content'>
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>{' '}
+                <picture>
+                  <img
+                    src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                    alt=''
+                  />
+                </picture>
+              </div>
+            </div>
+          </div>
+          <div className='second-images-line'>
+            <div className='second-images-line-content'>
+              <picture>
+                <img
+                  src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                  alt=''
+                />
+              </picture>
+              <picture>
+                <img
+                  src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                  alt=''
+                />
+              </picture>
+              <picture>
+                <img
+                  src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                  alt=''
+                />
+              </picture>
+              <picture>
+                <img
+                  src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                  alt=''
+                />
+              </picture>
+              <picture>
+                <img
+                  src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                  alt=''
+                />
+              </picture>
+              <picture>
+                <img
+                  src='https://images.pexels.com/photos/2145/sea-sunset-beach-couple.jpg?auto=compress&cs=tinysrgb&w=300'
+                  alt=''
+                />
+              </picture>
+            </div>
+          </div>
+        </section>
+        <section className='notes-container'></section>
+      </main>
+      <div className='days-container'></div>
+    </div>
+  );
+}
+
+export default App;
