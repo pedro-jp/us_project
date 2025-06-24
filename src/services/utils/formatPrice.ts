2fcf75fc70444d4538b03a0a1473b1ddf4b0f921
João Pedro <joaopedroc035@gmail.com>
2025-06-24 12:33:27 -0300

    fix: remove in-utils

--- src/services/utils/formatPrice.ts
+++
@@ -1,9 +0,0 @@
-const formatToBRL = (value: string) => {
-  const price = parseFloat(value.replace(/[^0-9]/g, '')) / 100;
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
-  }).format(price);
-};
-
-export default formatToBRL;
